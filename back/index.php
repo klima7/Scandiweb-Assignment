9edf44e85662b56ff8c05e6fbb863dd8fde7ad98
Lukasz Klimkiewicz <ukasz.klimkiewicz@proton.me>
2023-03-06 23:13:27 +0100

    Add CD for back

--- back/index.php
+++ back/index.php
@@ -3,6 +3,7 @@
         <title>My page</title>
     </head>
     <body>
-        <h1><?php echo "PHP works!" ?></h1>
+        <h1><?php echo "Backend CD works!" ?></h1>
+        <h1><?php echo 'PHP version: ' . phpversion() ?></h1>
     </body>
 </html>
