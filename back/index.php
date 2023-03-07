855d79f6119c0e1dcfda1868354dd3a3471b70ee
Lukasz Klimkiewicz <me@ukasz.com>
2023-03-07 01:03:47 +0100

    Add .thaccess

--- back/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-        <title>My page</title>
-    </head>
-    <body>
-        <h1><?php echo "Backend CD works!" ?></h1>
-        <h1><?php echo 'PHP version: ' . phpversion() ?></h1>
-    </body>
-</html>
