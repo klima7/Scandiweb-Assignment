4ec3b8f84c41d544b774a1a111160f6a0e421e97
Lukasz Klimkiewicz <me@ukasz.com>
2023-03-07 11:04:12 +0100

    Update .htaccess

--- back/api/user.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head>
-        <title>user</title>
-    </head>
-    <body>
-        <h1><?php echo 'Test' ?></h1>
-    </body>
-</html>
---
+++ back/index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Index</title>
+    </head>
+    <body>
+        <pre><?= print_r($GLOBALS) ?></pre>
+        <pre><?= print_r($_SERVER) ?></pre>
+    </body>
+</html>
