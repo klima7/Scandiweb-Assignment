5f38879ad79f978655f832dd384bb3546b3c50f2
Lukasz Klimkiewicz <me@ukasz.com>
2023-03-08 15:25:54 +0100

    Fix bug with .htaccess

--- back/index.php
+++ back/index.php
@@ -1,12 +1,15 @@
 <?php
 
-require __DIR__ . '/inc/bootstrap.php';
+//require __DIR__ . '/inc/bootstrap.php';
 
-use Framework\Router;
-use App\Controller\ProductController;
-use App\Controller\ProductsController;
+//use Framework\Router;
+//use App\Controller\ProductController;
+//use App\Controller\ProductsController;
 
-$router = new Router();
-$router->register('/api/products', new ProductsController());
-$router->register('/api/products/(\d+)', new ProductController());
-$router->handleRequest();
+$_SERVER;
+print_r($GLOBALS);
+
+//$router = new Router();
+//$router->register('/api/products', new ProductsController());
+//$router->register('/api/products/(\d+)', new ProductController());
+//$router->handleRequest();
