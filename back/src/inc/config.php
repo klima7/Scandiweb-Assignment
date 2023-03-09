828fe8246c7c9258295747c6a1c5651ba5921df8
Lukasz Klimkiewicz <me@ukasz.com>
2023-03-09 13:45:41 +0100

    Merge back and front

--- back/src/inc/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$db_host = 'mysql';
-$db_name = 'db';
-$db_user = 'user';
-$db_password = 'user';
--- src/src/App.js
+++ src/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world
+          Hel
         </p>
         <a
           className="App-link"
